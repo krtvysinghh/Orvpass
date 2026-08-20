50e92010cc631da9e927d33626a0b51e34519775
Kartavya Singh <kartxvyaa@gmail.com>
2026-08-20 21:22:02 +0530

    feat: create encrypted vault storage from cli init

--- cli/src/commands/init.rs
+++ cli/src/commands/init.rs
@@ -1,6 +1,7 @@
 use dialoguer::Password;
-use orvpass_core::vault::Vault;
+use std::path::PathBuf;
 use orvpass_core::crypto::SecretKey;
+use orvpass_core::vault::Vault;
 
 pub fn run() {
     let password = Password::new()
@@ -8,11 +9,25 @@ pub fn run() {
         .interact()
         .unwrap();
 
-    let key = SecretKey::from_password(password.as_bytes()).unwrap();
+    let key = SecretKey::from_password(password.as_bytes())
+        .expect("key derivation failed");
+
+    let path = PathBuf::from(
+        std::env::var("HOME")
+            .unwrap()
+    )
+    .join(".orvpass");
+
+    std::fs::create_dir_all(&path)
+        .expect("cannot create vault directory");
 
     let mut vault = Vault::new();
 
-    vault.initialize(&key).unwrap();
+    vault.initialize(&key)
+        .expect("vault initialization failed");
 
-    println!("Vault initialized");
+    vault.save(&key)
+        .expect("vault save failed");
+
+    println!("Vault initialized at {:?}", path);
 }
