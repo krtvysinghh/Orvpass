e2c741f438efe626679e3d1e0b9e9ccb5668038c
Kartavya Singh <kartxvyaa@gmail.com>
2026-08-22 21:06:32 +0530

    feat: add vault item persistence layer

--- core/src/vault/store.rs
+++ core/src/vault/store.rs
@@ -1,19 +1,29 @@
 use std::fs;
 use std::path::PathBuf;
 
+use crate::vault::item::VaultItem;
+
 fn path() -> PathBuf {
     dirs::home_dir()
         .unwrap()
         .join(".orvpass")
-        .join("vault.enc")
+        .join("vault.json")
 }
 
-pub fn save(data: &str) {
+pub fn save(items: &[VaultItem]) {
     let p = path();
     fs::create_dir_all(p.parent().unwrap()).unwrap();
+
+    let data = serde_json::to_string_pretty(items).unwrap();
     fs::write(p, data).unwrap();
 }
 
-pub fn load() -> String {
-    fs::read_to_string(path()).unwrap_or_default()
+pub fn load() -> Vec<VaultItem> {
+    let data = fs::read_to_string(path()).unwrap_or_default();
+
+    if data.is_empty() {
+        Vec::new()
+    } else {
+        serde_json::from_str(&data).unwrap_or_default()
+    }
 }
