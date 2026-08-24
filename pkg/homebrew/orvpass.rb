11c96c45a6574ff287decc1394c43038af113ff9
Kartavya Singh <kartxvyaa@gmail.com>
2026-08-24 19:20:42 +0530

    fix: rename app to Orvpass everywhere, ad-hoc sign macOS, fix identifier

--- pkg/homebrew/orvpass.rb
+++ pkg/homebrew/orvpass.rb
@@ -1,18 +1,18 @@
 cask "orvpass" do
-  version "4.0.7"
-  sha256 "a77d0ccc108609748b13ec0003b68cdedc6cd14d3b1827c44debceea9ca25a4c"
+  version "4.0.8"
+  sha256 :no_check
 
-  url "https://github.com/krtvysinghh/Orvpass/releases/download/v#{version}/app_0.1.0_aarch64.dmg"
+  url "https://github.com/krtvysinghh/Orvpass/releases/download/v#{version}/Orvpass_#{version}_aarch64.dmg"
   name "Orvpass"
   desc "Lightning-fast, highly secure, natively designed password manager"
   homepage "https://github.com/krtvysinghh/Orvpass"
 
-  app "app.app", target: "Orvpass.app"
+  app "Orvpass.app"
 
   zap trash: [
     "~/Library/Application Support/Orvpass",
     "~/Library/Caches/Orvpass",
-    "~/Library/Preferences/com.krtvysingh.app.plist",
-    "~/Library/Saved Application State/com.krtvysingh.app.savedState",
+    "~/Library/Preferences/com.krtvysingh.orvpass.plist",
+    "~/Library/Saved Application State/com.krtvysingh.orvpass.savedState",
   ]
 end
