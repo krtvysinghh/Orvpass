eb954ddfae80d292ad2617b933a598cd1d78754b
Kartavya Singh <kartxvyaa@gmail.com>
2026-09-02 22:34:01 +0530

    test(import_export): add unit tests for offline HTML decryptor export generation

---
+++ core/tests/html_export_tests.rs
@@ -0,0 +1,14 @@
+use orvpass_core::models::{VaultItem, ItemType, ItemData, LoginData};
+use orvpass_core::import_export::exporters::html::export_standalone_html;
+
+#[test]
+fn test_html_export_contains_structure() {
+    let items = vec![VaultItem::new(ItemType::Login, "Test Service", ItemData::Login(LoginData {
+        username: Some("user".into()),
+        password: Some("pass".into()),
+        urls: vec![]
+    }))];
+    let html = export_standalone_html(&items);
+    assert!(html.contains("Orvpass Emergency Vault"));
+    assert!(html.contains("Test Service"));
+}
